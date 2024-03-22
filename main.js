835aa0724186ff65bf3ab56491d8c1ae75172e76
Maxime Peretti <maxime.peretti@heig-vd.ch>
2024-03-22 13:36:53 +0100

    Finition 1er view port et burger cursor

--- main.js
+++ main.js
@@ -37,6 +37,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 12, 0);
 
+
 //Create a control to move arround the object
 let mouseX = window.innerWidth / 2;
 let mouseY = window.innerHeight / 2;
@@ -82,13 +83,13 @@ document.getElementById("container3D").appendChild(renderer.domElement);
 //set how far the camera will be from the object
 camera.position.z = objToRender === 'burger' ? 25 : 500;
 
-//Add light to see the model
-// const light = new THREE.DirectionalLight(0xffffff, 1); //Color and intensity
-// light.position.set(500, 500, 500); //Top-left-ish
-// light.castShadow = true; //Enable shadow
-// scene.add(light);
+// Add light to see the model
+const light = new THREE.DirectionalLight(0xffffff, 1.1);
+light.position.set(0, 200, 0); //Top
+light.castShadow = true; //Enable shadow
+scene.add(light);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 1.5); // Couleur blanche avec une intensité de 0.5
+const ambientLight = new THREE.AmbientLight(0xffffff, 1.05);
 scene.add(ambientLight);
 
 //Create a control to move arround the object
@@ -96,12 +97,6 @@ if (objToRender === 'burger') {
     controls = new OrbitControls(camera, renderer.domElement);
     controls.enableZoom = false; // Disable zoom on scroll
 }
-
-// function rotateObject(object, degreeX=0, degreeY=0, degreeZ=0) {
-//     object.rotateX(THREE.Math.degToRad(degreeX));
-//     object.rotateY(THREE.Math.degToRad(degreeY));
-//     object.rotateZ(THREE.Math.degToRad(degreeZ));
-//  }
  
 //  // usage:
 //  rotateObject(burger, 40, 30, 20);
@@ -154,6 +149,7 @@ updateNumber();
 // Appelez la fonction updateNumber toutes les secondes (1000 millisecondes)
 setInterval(updateNumber, 23);
 
+
 //LOADING SCREEN ----------------------------------------------------------------------------------------------------------------------------
 // Déclarez votre élément de préchargement avec un nom de variable différent
 let preloader = document.querySelector('#preloader');
@@ -162,3 +158,4 @@ window.addEventListener('load', function () {
     preloader.style.display = 'none';
 });
 
+//CURSOR ----------------------------------------------------------------------------------------------------------------------------
