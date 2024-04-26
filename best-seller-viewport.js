a6fe7f49f0efaec041f6a9fcd911df198ed7cd02
Léo Winterhalter <leo.winterhalter@heig-vd.ch>
2024-04-26 11:59:11 +0200

    Ajout des animations dans le viewport best-seller-viewport

--- best-seller-viewport.js
+++ best-seller-viewport.js
@@ -14,6 +14,61 @@ function extractFoodPairings(jsonData) {
         }));
 }
 
+function drawScrollLine() {
+    const section = d3.select('.best_seller-section');
+    let svgContainer = section.select('.line-chart-svg');
+    const impactElement = document.querySelector('.impact-sales');
+
+    const height = 1000;
+
+    if (svgContainer.empty()) {
+        svgContainer = section.append('svg')
+            .attr('width', '100%')
+            .attr('height', height * 1)
+            .attr('class', 'line-chart-svg');
+    }
+
+    const line = svgContainer.append('line')
+        .attr('x1', 0)
+        .attr('y1', 0)
+        .attr('x2', 0)
+        .attr('y2', height)
+        .attr('stroke', getComputedStyle(document.documentElement).getPropertyValue('--text-icons-color'))
+        .attr('stroke-width', 10)
+        .attr('stroke-dasharray', height)
+        .attr('stroke-dashoffset', height);
+
+        function updateLine() {
+            const scrollPosition = window.pageYOffset + window.innerHeight;
+            const impactPosition = impactElement.getBoundingClientRect().top + window.pageYOffset;
+            const height = line.node().getBoundingClientRect().height;
+            const scrollPercentage = (scrollPosition - impactPosition) / height;
+            const newOffset = Math.max(height - (height * scrollPercentage * 0.8), 0);
+            line.attr('stroke-dashoffset', newOffset);
+        
+            // Vérifier si la ligne est complètement révélée
+            if (newOffset === 0 && svgContainer.select('circle').empty()) {
+                // Supprimer l'écouteur d'événement de défilement une fois que la ligne est complètement déployée
+                window.removeEventListener('scroll', updateLine);                // Ajouter un cercle seulement s'il n'existe pas déjà
+                const circle = svgContainer.append('circle')
+                    .attr('cx', 0)  // Coordonnée x du centre du cercle
+                    .style('position', "absolute")
+                    .attr('cy', 1000)  // Coordonnée y du centre du cercle
+                    .attr('r', 0.5)  // Commencez avec un rayon de 0 pour être invisible
+                    .attr('fill', getComputedStyle(document.documentElement).getPropertyValue('--text-icons-color'));  // Couleur du cercle
+        
+                // Animer le cercle pour agrandir de 0 à une taille spécifique
+                circle.transition()
+                    .duration(1000)  // Durée de l'animation
+                    .attr('r', 30);  // Rayon final du cercle
+            }
+        }
+        
+
+    window.addEventListener('scroll', updateLine);
+}
+
+
 function GenerateLoadFood() {
     fetch('../datas/burger_king_statistics.json') // Assurez-vous que ce chemin est correct
         .then(response => response.json())
@@ -103,6 +158,7 @@ function GenerateLoadFood() {
 
             
     });
+    drawScrollLine();
 }
 
 export { extractFoodPairings, GenerateLoadFood };
