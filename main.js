2c13afaaf6901ee23a31c4b3563bafd574d03fc4
Sami Gilliand <sami.gilliand@heig-vd.ch>
2024-05-01 13:23:03 +0200

    Implementation de la dashboard + Modifications entières des ingrédients en Bubble Chart - CSS + JS + Modification HTML

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import { make3DBurger } from "./3d-viewport.js";
 import { countBurgers } from './burgers-count.js';
 import { GenerateLoadFood, extractFoodPairings } from './best-seller-viewport.js';
-import { generateIngredientsList } from "./ingredients-viewport.js";
+// import { generateIngredientsList } from "./ingredients-viewport.js";
+import { generateIngredientsBubbleChart } from "./ingredients-viewport.js";
 import { initializeDashboard } from "./dashboard-viewport.js";
 
 // 0.1 - Chargement des fichiers JSON & CSV
@@ -44,7 +45,8 @@ GenerateLoadFood();
 
 // Troisième ViewPort - Ingrédients ------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-generateIngredientsList();
+// generateIngredientsList();
+generateIngredientsBubbleChart();
 
 // Quatrième ViewPort - Pays ------------------------------------------------------------------------------------------------------------------------------------------------------------
 
